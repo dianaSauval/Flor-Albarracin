03c6d2055d5e73a7c2b506eb5dbe33eb08416218
Diana Sauval <dianasauval@hotmail.com>
2026-08-07 11:15:48 +0200

    cambio de fechas

--- src/data/dates.js
+++ src/data/dates.js
@@ -1,8 +1,8 @@
 export const dates = [
   {
-    type: "weekly",
-    month: "current",
-    days: ["Wed"],
+    type: "date",
+    month: "2026-08",
+    dateLabel: "6 de agosto",
     time: "21:00",
     city: "Buenos Aires, AR",
     venue: "Lucrecia vinos",
@@ -10,7 +10,52 @@ export const dates = [
     addressFull:
       "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
     mapUrl:
-      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c?hl=es&entry=ttu&g_ep=EgoyMDI2MDIxMS4wIKXMDSoASAFQAw%3D%3D",
+      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
+    event: "Show",
+    ticketUrl: "",
+  },
+  {
+    type: "date",
+    month: "2026-08",
+    dateLabel: "20 de agosto",
+    time: "21:00",
+    city: "Buenos Aires, AR",
+    venue: "Lucrecia vinos",
+    addressShort: "Av. Jorge Newbery 3519 (CABA)",
+    addressFull:
+      "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
+    mapUrl:
+      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
+    event: "Show",
+    ticketUrl: "",
+  },
+  {
+    type: "date",
+    month: "2026-09",
+    dateLabel: "3 de septiembre",
+    time: "21:00",
+    city: "Buenos Aires, AR",
+    venue: "Lucrecia vinos",
+    addressShort: "Av. Jorge Newbery 3519 (CABA)",
+    addressFull:
+      "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
+    mapUrl:
+      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
+    event: "Show",
+    ticketUrl: "",
+  },
+  {
+    type: "date",
+    month: "2026-09",
+    dateLabel: "17 de septiembre",
+    time: "21:00",
+    city: "Buenos Aires, AR",
+    venue: "Lucrecia vinos",
+    addressShort: "Av. Jorge Newbery 3519 (CABA)",
+    addressFull:
+      "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
+    mapUrl:
+      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
     event: "Show",
     ticketUrl: "",
   },

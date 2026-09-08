e32edae8e550b604df8523ff2acb17d739be3c0a
Diana Sauval <dianasauval@hotmail.com>
2026-09-08 19:14:44 +0200

    cambios de horarios

--- src/data/dates.js
+++ src/data/dates.js
@@ -1,36 +1,6 @@
 export const dates = [
   {
     type: "date",
-    month: "2026-08",
-    dateLabel: "6 de agosto",
-    time: "21:00",
-    city: "Buenos Aires, AR",
-    venue: "Lucrecia vinos",
-    addressShort: "Av. Jorge Newbery 3519 (CABA)",
-    addressFull:
-      "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
-    mapUrl:
-      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
-    event: "Show",
-    ticketUrl: "",
-  },
-  {
-    type: "date",
-    month: "2026-08",
-    dateLabel: "20 de agosto",
-    time: "21:00",
-    city: "Buenos Aires, AR",
-    venue: "Lucrecia vinos",
-    addressShort: "Av. Jorge Newbery 3519 (CABA)",
-    addressFull:
-      "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
-    mapUrl:
-      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
-    event: "Show",
-    ticketUrl: "",
-  },
-  {
-    type: "date",
     month: "2026-09",
     dateLabel: "3 de septiembre",
     time: "21:00",
@@ -48,6 +18,23 @@ export const dates = [
     type: "date",
     month: "2026-09",
     dateLabel: "17 de septiembre",
+    time: "21:00",
+    city: "Buenos Aires, AR",
+    venue: "Lucrecia vinos",
+    addressShort: "Av. Jorge Newbery 3519 (CABA)",
+    addressFull:
+      "Av. Jorge Newbery 3519, C1427 Cdad. Autónoma de Buenos Aires, Argentina",
+    mapUrl:
+      "https://www.google.com/maps/place/Lucrecia+vinos/@-34.5829598,-58.4516693,17z/data=!3m1!4b1!4m6!3m5!1s0x95bcb5458d8911d7:0x7aaec8f95f9824d5!8m2!3d-34.5829642!4d-58.4490944!16s%2Fg%2F11n10k5c9c",
+    event: "Show",
+    ticketUrl: "",
+  },
+
+  // Lucrecia queda fijo todos los lunes y miércoles
+  {
+    type: "weekly",
+    month: "current",
+    days: ["Mon", "Wed"],
     time: "21:00",
     city: "Buenos Aires, AR",
     venue: "Lucrecia vinos",
